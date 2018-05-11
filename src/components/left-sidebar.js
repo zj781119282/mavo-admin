c9240f39e8fc6cf1a8dd8d2f7ab38d14ffbd6ab4
ZJ <zj781119282@163.com>
2018-05-11 18:54:08 +0800

    little fix

--- src/components/left-sidebar.js
+++ src/components/left-sidebar.js
@@ -2,6 +2,9 @@ import $ from 'jquery'
 
 $(() => {
   const page = location.pathname;
-  const currentPage = page.split('/')[1].split('.')[0];
+  let currentPage = page.split('/')[1].split('.')[0];
+  if (!currentPage) {
+    currentPage = 'index';
+  }
   $(`.left-side-inner .nav .${currentPage}`).addClass('active');
 })
